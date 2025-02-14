11d518450529311664ae8f1ade3af6f9688d85b9
Eliot Menoret <eliot.menoret@etu.univ-cotedazur.fr>
2025-02-14 09:47:19 +0100

    suite

---
+++ src/Filter1.java
@@ -0,0 +1,23 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Filter1 {
+
+    public static List<Person> filterByAge(List<Person> people, int minAge) {
+        return people.parallelStream()
+                .filter(p -> p.age() >= minAge)
+                .collect(Collectors.toList());
+    }
+
+    public static List<Person> filterByBirthPlace(List<Person> people, int departmentId) {
+        return people.parallelStream()
+                .filter(p -> p.departmentId() == departmentId)
+                .collect(Collectors.toList());
+    }
+
+    public static List<Person> filterByLastName(List<Person> people, String lastName) {
+        return people.parallelStream()
+                .filter(p -> p.lastName().equalsIgnoreCase(lastName))
+                .collect(Collectors.toList());
+    }
+}
